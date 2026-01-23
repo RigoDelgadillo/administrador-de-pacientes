d888c9b078efa12cf4ba99627fd32263270a981c
Rigo Delgadillo <rigodelgadillo10@gmail.com>
2026-01-23 07:43:03 -0600

    se agregaron los botones de editar y eliminar

--- src/components/PatientDetail.tsx
+++ src/components/PatientDetail.tsx
@@ -17,6 +17,21 @@ export default function PatientDetail({ patient }: PatientDetailProps) {
         label="Fecha de alta:"
         data={patient.date.toString()}
       />
+
+      <div className="flex justify-between gap-3 mt-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg hover:cursor-pointer"
+        >
+          Editar
+        </button>
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg hover:cursor-pointer"
+        >
+          Editar
+        </button>
+      </div>
     </div>
   );
 }
