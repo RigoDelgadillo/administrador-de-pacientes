5abbfe8a6f7b9b06e76e151b22c554dfcbf321b1
Rigo Delgadillo <rigodelgadillo10@gmail.com>
2026-01-23 07:29:21 -0600

    La informacion del paciente se muestra en pantalla

--- src/components/PatientDetail.tsx
+++ src/components/PatientDetail.tsx
@@ -1,9 +1,22 @@
 import type { Patient } from "../interfaces";
+import PatientDetailItem from "./PatientDetailItem";
 
 type PatientDetailProps = {
   patient: Patient;
 };
 
 export default function PatientDetail({ patient }: PatientDetailProps) {
-  return <div>{patient.name}</div>;
+  return (
+    <div className="mx-10 my-5 px-10 p-5 bg-white shadow-md rounded-xl">
+      <PatientDetailItem label="ID:" data={patient.id} />
+      <PatientDetailItem label="Nombre:" data={patient.name} />
+      <PatientDetailItem label="Propietario:" data={patient.caretaker} />
+      <PatientDetailItem label="Email:" data={patient.email} />
+      <PatientDetailItem label="Síntomas:" data={patient.symptoms} />
+      <PatientDetailItem
+        label="Fecha de alta:"
+        data={patient.date.toString()}
+      />
+    </div>
+  );
 }
